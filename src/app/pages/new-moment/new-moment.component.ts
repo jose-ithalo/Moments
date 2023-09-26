470610b76645406053fc967c01a4f4c0466d6676
José Ithalo <jose_ithalo@hotmail.com>
2023-09-26 16:36:11 -0300

    Método para registrar momento desenvolvido

--- src/app/pages/new-moment/new-moment.component.ts
+++ src/app/pages/new-moment/new-moment.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+import { MomentsService } from 'src/app/sercices/moments.service';
+import Moment from 'src/app/interfaces/Moments';
+
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
   styleUrls: ['./new-moment.component.css']
 })
 export class NewMomentComponent {
+
+  btnText: string = 'Compartilhar!';
+
+  constructor(private momentService: MomentsService) { }
+
+  async createHandler(moment: Moment) {
+    console.log(moment);
+
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    this.momentService.createMoment(formData).subscribe();
+
+  }
 
 }
