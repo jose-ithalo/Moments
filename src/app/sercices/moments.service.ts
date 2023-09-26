3b0eb282162c25c2f769dac6eab2256e8c4746fe
José Ithalo <jose_ithalo@hotmail.com>
2023-09-26 16:39:33 -0300

    Integração do serviço com a api realizada

--- src/app/sercices/moments.service.ts
+++ src/app/sercices/moments.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import Moment from '../interfaces/Moments';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MomentsService {
 
-  constructor() { }
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
+
+  constructor(private http: HttpClient) { }
+
+  createMoment(formDate: FormData): Observable<FormData> {
+
+    return this.http.post<FormData>(this.apiUrl, formDate);
+  }
 }
