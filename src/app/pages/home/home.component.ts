791f050cdcf597b06651be9722c1351f9420f941
José Ithalo <jose_ithalo@hotmail.com>
2023-10-05 21:26:28 -0300

    Ação para listar momentos inserida na página

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { MomentsService } from 'src/app/services/moments.service';
+import Moment from 'src/app/interfaces/Moments';
+import { environment } from 'src/environments/environment';
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  allMoments: Moment[] = [];
+  chosenMoments: Moment[] = [];
+  baseUrl = environment.baseApiUrl;
+
+  constructor(private momentService: MomentsService) { }
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items) => {
+      const data: Moment[] = items.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
+      });
+
+      this.allMoments = data;
+      this.chosenMoments = data;
+    });
+  }
 
 }
