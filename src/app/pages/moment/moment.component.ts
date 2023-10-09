2b23eeb9dd46ecbc7db5fac47c0d502dc4fcf27c
José Ithalo <jose_ithalo@hotmail.com>
2023-10-09 17:04:50 -0300

    Método de deletar momento inserido

--- src/app/pages/moment/moment.component.ts
+++ src/app/pages/moment/moment.component.ts
@@ -4,6 +4,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 import { MomentsService } from 'src/app/services/moments.service';
+import { MessagesService } from 'src/app/services/messages.service';
 import Moment from 'src/app/interfaces/Moments';
 import { environment } from 'src/environments/environment';
 
@@ -19,11 +20,26 @@ export class MomentComponent implements OnInit {
   iconEdit = faEdit;
   iconDelete = faTimes;
 
-  constructor(private momentService: MomentsService, private route: ActivatedRoute) { }
+  constructor(
+    private momentService: MomentsService,
+    private messsagesService: MessagesService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     const id: number = Number(this.route.snapshot.paramMap.get('id'));
 
     this.momentService.getOneMoment(id).subscribe((item) => (this.moment = item.data));
+  }
+
+  removeHandler(id: number): void {
+
+    this.momentService.deleteMoment(id).subscribe();
+
+    this.messsagesService.add('Momento excluído com sucesso!');
+
+    this.router.navigate(['/']);
+
   }
 }
